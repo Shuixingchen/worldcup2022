adf99baf4c7388f48e67b10ac10f72b13c22b75a
admin <admin@btcmdeMacBook-Pro.local>
2022-11-21 17:39:52 +0800

    aa

--- src/Pages/Header/index.js
+++ src/Pages/Header/index.js
@@ -10,7 +10,7 @@ function Header(){
             </a>
     
             <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-              <li><a href="" className="nav-link px-2 text-secondary">World Cup 2022</a></li>
+              <li><a href="/" className="nav-link px-2 text-secondary">World Cup 2022</a></li>
             </ul>
             {/* 连接钱包 */}
             <Metamask/>
